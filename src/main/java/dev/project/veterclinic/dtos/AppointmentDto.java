f51d748ae60b1d5e27081fad9380e29c61958a03
NelliYanchuk <yanchuk.nelli@gmail.com>
2024-12-05 14:27:25 +0100

    ci:AppDTO

---
+++ src/main/java/dev/project/veterclinic/dtos/AppointmentDto.java
@@ -0,0 +1,17 @@
+package dev.project.veterclinic.dtos;
+
+import dev.project.veterclinic.models.AppointmentStatus;
+import dev.project.veterclinic.models.AppointmentType;
+
+import java.time.LocalDateTime;
+
+public record AppointmentDto(
+    int id,
+    LocalDateTime date,
+    int petId,
+    AppointmentType type,
+    String reason,
+    AppointmentStatus status,
+    int ownerId
+) {
+}
