f0b5a46538dc0e4b0663cd07e8f7002b8867d4b8
Kare <kareliz01@gmail.com>
2024-12-05 16:26:35 +0100

    class mapping

--- src/main/java/dev/project/veterclinic/models/Treatment.java
+++ src/main/java/dev/project/veterclinic/models/Treatment.java
@@ -1,51 +1,79 @@
+package dev.project.veterclinic.models;
+
 import java.time.LocalDate;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "TREATMENTS")
 public class Treatment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
+
+    @Column(name = "date")
     private LocalDate date;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type")
     private TreatmentType type;
+
+    @Column(name = "note")
     private String note;
 
-    public enum TreatmentType{
+    public enum TreatmentType {
         MEDICATION, THERAPY, SURGERY, CHECKUP
     }
-public Treatment(int id, LocalDate date, TreatmentType type, String note) {
-    this.id = id;
-    this.date = date;
-    this.type = type;
-    this.note = note;
-}
 
-public void setId(int id) {
-    this.id = id;
-}
+    public Treatment() {
+    }
 
-public int getId() {
-    return Id;
-}
+    public Treatment(int id, LocalDate date, TreatmentType type, String note) {
+        this.id = id;
+        this.date = date;
+        this.type = type;
+        this.note = note;
+    }
 
-public void setDate(LocalDate date) {
-    this.date = date;
-}
+    // Getters and setters
+    public int getId() {
+        return id;
+    }
 
-public LocalDate getDate(){
-    return date;
-}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-public void setType(TreatmentType type) {
-    this.type = type;
-}
+    public LocalDate getDate() {
+        return date;
+    }
 
-public TreatmentType getType() {
-    return type;
-}
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
 
-public void setNote(String note) {
-    this.note = note;
-}
+    public TreatmentType getType() {
+        return type;
+    }
 
-public String getNote () {
-    return note;
-}
+    public void setType(TreatmentType type) {
+        this.type = type;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
 }
