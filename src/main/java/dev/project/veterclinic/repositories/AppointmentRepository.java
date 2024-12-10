11923f12457c9323c4e0adce36d4ba0ffdcc655f
NelliYanchuk <yanchuk.nelli@gmail.com>
2024-12-10 16:34:01 +0100

    feat:get app by user id, update app by user id and app id

--- src/main/java/dev/project/veterclinic/repositories/AppointmentRepository.java
+++ src/main/java/dev/project/veterclinic/repositories/AppointmentRepository.java
@@ -2,7 +2,9 @@ package dev.project.veterclinic.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import dev.project.veterclinic.models.Appointment;
+import java.util.List;
 
 public interface AppointmentRepository extends JpaRepository<Appointment, Integer> {
-
+    // Query method to find appointments by ownerId and order by date
+    List<Appointment> findByOwnerIdOrderByDateAsc(int ownerId);
 }
