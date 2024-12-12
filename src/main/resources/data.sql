f614d4d2c9b2c11bb47e91b10d64065c9c4b5359
abdias labrador <abdiaslabrador@gmail.com>
2024-12-12 12:08:15 +0100

    fix sql problem

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -94,21 +94,3 @@ VALUES
   (18, 2, '2023-07-29', 'Mia', 'Female', 'CAT'),
   (19, 3, '2022-12-21', 'Duke', 'Male', 'DOG'),
   (20, 1, '2021-06-16', 'Sophie', 'Female', 'DOG');
-
--- table APPOINTMENT
-/*INSERT INTO appointments (id, date, pet_id, type, reason, status, owner_id) VALUES (default, '2020-03-15 09:30:00', 1, 'URGENT', 'bleeding', 'PASSED', 6);
-INSERT INTO appointments (id, date, pet_id, type, reason, status, owner_id) VALUES (default, '2022-04-19 10:30:00', 3, 'ORDINARY', 'sore eyes', 'PASSED', 9);
-INSERT INTO appointments (id, date, pet_id, type, reason, status, owner_id) VALUES (default, '2025-03-13 01:00:00', 5, 'ORDINARY', 'check-up', 'PENDING', 2);
-INSERT INTO appointments (id, date, pet_id, type, reason, status, owner_id) VALUES (default, '2023-09-05 07:00:00', 2, 'ORDINARY', 'stomach pain', 'PASSED', 9);
-INSERT INTO appointments (id, date, pet_id, type, reason, status, owner_id) VALUES (default, '2020-12-01 13:30:00', 6, 'ORDINARY', 'rash', 'CANCELED', 3);
-INSERT INTO appointments (id, date, pet_id, type, reason, status, owner_id) VALUES (default, '2020-08-18 11:30:00', 14, 'ORDINARY', 'excessive thirst', 'PASSED', 5);
-INSERT INTO appointments (id, date, pet_id, type, reason, status, owner_id) VALUES (default, '2021-11-09 18:00:00', 4, 'URGENT', 'vomiting', 'PASSED', 9);
-INSERT INTO appointments (id, date, pet_id, type, reason, status, owner_id) VALUES (default, '2022-01-19 14:00:00', 7, 'URGENT', 'vomiting', 'PASSED', 3);
-INSERT INTO appointments (id, date, pet_id, type, reason, status, owner_id) VALUES (default, '2023-09-18 05:30:00', 15, 'URGENT', 'allergic reaction', 'PASSED', 1);
-INSERT INTO appointments (id, date, pet_id, type, reason, status, owner_id) VALUES (default, '2025-10-28 19:30:00', 5, 'ORDINARY', 'check-up', 'PENDING', 9);
-INSERT INTO appointments (id, date, pet_id, type, reason, status, owner_id) VALUES (default, '2025-08-17 08:30:00', 2, 'URGENT', 'check-up', 'CANCELED', 8);
-INSERT INTO appointments (id, date, pet_id, type, reason, status, owner_id) VALUES (default, '2023-09-14 20:00:00', 14, 'ORDINARY', 'cough', 'PASSED', 4);
-INSERT INTO appointments (id, date, pet_id, type, reason, status, owner_id) VALUES (default, '2022-11-14 22:00:00', 5, 'ORDINARY', 'skin infection', 'PASSED', 2);
-INSERT INTO appointments (id, date, pet_id, type, reason, status, owner_id) VALUES (default, '2025-04-29 06:30:00', 15, 'URGENT', 'check-up', 'PENDING', 7);
-INSERT INTO appointments (id, date, pet_id, type, reason, status, owner_id) VALUES (default, '2024-02-18 20:30:00', 9, 'ORDINARY', 'injury', 'PASSED', 9);
-INSERT INTO appointments (id, date, pet_id, type, reason, status, owner_id) VALUES (default, '2023-01-02 11:30:00', 15, 'ORDINARY', 'injury', 'PASSED', 9);*/
